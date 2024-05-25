cb7bd47cdba78557991986c8d77ca23a665e7681
yqhuang1 <yqhuang1@qq.com>
2024-05-25 18:19:46 +0800

    red book

---
+++ lib/pages/mine/mine_controller.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../api/api_client.dart';
+import '../../constants/pages.dart';
+import '../../models/card_data.dart';
+
+class MineController extends GetxController with GetSingleTickerProviderStateMixin {
+  late TabController tabController;
+  List<CardData> data = [];
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    tabController = TabController(length: 3, vsync: this);
+    getIndexData();
+  }
+
+  void getIndexData() {
+    ApiClient().getIndexData().then((response) {
+      data = response;
+      update();
+    });
+  }
+
+  void openIndexDetailPage(int id) {
+    Get.toNamed(Pages.indexDetail, arguments: {"id": id});
+  }
+}
